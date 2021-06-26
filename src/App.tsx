477a222f59fa48c8507ce1360aba41a3ecf2bb06
Tobias <tobiascm87@gmail.com>
2021-06-26 10:15:10 -0300

    Aula 2 (Parcial) | Criando Páginas

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
+import { BrowserRouter, Route } from 'react-router-dom'
+
+import { Home } from './pages/home'
+import { NewRoom } from './pages/newRoom'
+
+import  './styles/global.scss'
+
 function App() {
   return (
-    <h1>Hello, World</h1>
+    <BrowserRouter>
+      <Route path="/" exact component={Home} />
+      <Route path="/rooms/new" component={NewRoom} />
+    </BrowserRouter>
   )
 }
 
